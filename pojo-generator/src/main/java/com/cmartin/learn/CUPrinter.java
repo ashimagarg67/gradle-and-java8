8104dbfe06059973b8422ffa7af1a506960dd595
Carlos Martin <cmarting@gmail.com>
2017-05-12 22:10:24 +0200

    added simple code visitors

--- pojo-generator/src/main/java/com/cmartin/learn/CUPrinter.java
+++ pojo-generator/src/main/java/com/cmartin/learn/CUPrinter.java
@@ -2,6 +2,9 @@ package com.cmartin.learn;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
 
@@ -16,7 +19,35 @@ public class CUPrinter {
         // parse the file
         CompilationUnit cu = JavaParser.parse(in);
 
+        // visit and print the methods names
+        new MethodVisitor().visit(cu, null);
+
+        new VariableVisitor().visit(cu, null);
+
         // prints the resulting compilation unit to default system output
-        System.out.println(cu.toString());
+        // System.out.println(cu.toString());
+    }
+
+
+    /**
+     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     */
+    private static class MethodVisitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            /* here you can access the attributes of the method.
+             this method will be called for all methods in this
+             CompilationUnit, including inner class methods */
+            System.out.println(n.getName());
+            super.visit(n, arg);
+        }
+    }
+
+    private static class VariableVisitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(VariableDeclarator v, Void arg) {
+            System.out.println(v.getType());
+            super.visit(v, arg);
+        }
     }
 }
