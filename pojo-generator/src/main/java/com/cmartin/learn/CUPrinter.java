4b5f2fb9df89dc0e45487b0e54650ca2c02f01c3
Carlos Martin <cmarting@gmail.com>
2017-05-13 13:33:41 +0200

    added function to retrieve all DTO elements; added Class visitor

--- pojo-generator/src/main/java/com/cmartin/learn/CUPrinter.java
+++ pojo-generator/src/main/java/com/cmartin/learn/CUPrinter.java
@@ -2,17 +2,36 @@ package com.cmartin.learn;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 /**
  * Created by cmartin on 12/05/2017.
  */
 public class CUPrinter {
-    public static void main(String[] args) throws Exception {
+
+    public static final String SMC_PATH = "/Users/cmartin/projects/bbva-2017/stash/customerbudgets/src/main/java";
+    private static final String JAVA_EXTENDSION = ".java";
+
+    public static void main(String[] args) throws IOException {
+        //readAllFiles(SMC_PATH);
+
+        readAllDirectories(SMC_PATH).forEach(path ->
+                printVariables(path.getPath()));
+
+        if (true) return;
         // creates an input stream for the file to be parsed
         FileInputStream in = new FileInputStream("/tmp/Money.java");
 
@@ -28,6 +47,44 @@ public class CUPrinter {
         // System.out.println(cu.toString());
     }
 
+    private static void printVariables(final String path) {
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(path);
+            CompilationUnit cu = JavaParser.parse(in);
+            new ClassVisitor().visit(cu, null);
+            new VariableVisitor().visit(cu, null);
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Stream<File> readAllDirectories(final String path) throws IOException {
+
+        return Files.walk(Paths.get(path), FileVisitOption.FOLLOW_LINKS)
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .filter(f -> f.getPath().contains("dto"))
+                .filter(f -> f.getName().endsWith(JAVA_EXTENDSION));
+        //.forEach(System.out::println);
+    }
+
+
+    private static void readAllFiles(final String path) throws IOException {
+        Files.list(Paths.get(path))
+                .filter(Files::isRegularFile)
+                //.filter(f -> f.getFileName().endsWith(".java"))
+                .forEach(System.out::println);
+    }
+
+    private static class ClassVisitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(ClassOrInterfaceDeclaration c, Void arg) {
+            System.out.println(c.getNameAsString());
+            super.visit(c, arg);
+        }
+    }
 
     /**
      * Simple visitor implementation for visiting MethodDeclaration nodes.
